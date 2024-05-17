0256b29f5190200509730c28b2c3fd8e62c6d3fc
Luka <lmebo21@freeuni.edu.ge>
2024-05-17 19:52:22 +0400

    Added Increasing Array Solution

---
+++ IntroductoryProblems/IncreasingArray.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(),(x).end()
+
+void solve() {
+   int n; cin >> n;
+   int arr[n];
+   for(int i = 0; i < n; i++){
+        cin >> arr[i];
+   }
+    long long cnt = 0;
+    for(int i = 1; i < n; i++){
+        if(arr[i] < arr[i-1]){
+            cnt += arr[i-1] - arr[i];
+            arr[i] = arr[i-1];
+        }
+    }
+    cout << cnt << endl;
+    
+}
+
+int main() {
+    int t; t = 1;
+    while (t--) {
+        solve();
+    }
+}
--- IntroductoryProblems/WeirdAlgorithm.cpp
+++ IntroductoryProblems/WeirdAlgorithm.cpp
@@ -4,13 +4,18 @@ using namespace std;
 #define all(x) (x).begin(),(x).end()
 
 void solve() {
-    long long n; cin >> n; cout << n << " ";
-    while(n != 1){
-        if(n % 2){
-            n = 3*n + 1;
-        } else n /= 2;
-        cout << n << " ";
-    }
+   string str; cin >> str;
+   int n = str.size();
+   int answ = 1;
+   int cnt = 1;
+   for(int i = 1; i < n; i++){
+        if(str[i] == str[i-1]){
+            cnt++;
+            answ = max(answ, cnt);
+        } else cnt = 1;
+   }
+   cout << answ << endl;
+    
 }
 
 int main() {
