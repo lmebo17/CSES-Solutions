6975394f3691158084785f7a54cd5ce68f21f000
Luka <lmebo21@freeuni.edu.ge>
2024-05-17 20:55:28 +0400

    added Two Knight Solution

---
+++ IntroductoryProblems/TwoKnight.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(),(x).end()
+
+long long calculate(long long k){
+    long long total = (k*k) * (k*k - 1)/2;
+    long long attack = 4 * (k-1) * (k-2);
+    return total - attack;
+}
+
+void solve() {
+    long long n; cin >> n;
+    for(long long i = 1; i <= n; i++){
+        cout << calculate(i) << endl;
+    } 
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
