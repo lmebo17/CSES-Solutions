55f30b62b292bfff957dc18b0d2f7ff9b260091b
Luka <lmebo21@freeuni.edu.ge>
2024-05-17 19:46:59 +0400

    Added Repetitions Solution

---
+++ IntroductoryProblems/Repetitions.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(),(x).end()
+
+void solve() {
+   string str; cin >> str;
+   int n = str.size();
+   int answ = 1;
+   int cnt = 1;
+   for(int i = 1; i < n; i++){
+        if(str[i] == str[i-1]){
+            cnt++;
+            answ = max(answ, cnt);
+        } else cnt = 1;
+   }
+   cout << answ << endl;
+    
+}
+
+int main() {
+    int t; t = 1;
+    while (t--) {
+        solve();
+    }
+}
